e80224b3010fe3b089ceff6eab692e85b770d0c7
ahori@riken.jp <ahori@riken.jp>
2020-11-17 11:42:36 +0900

    Fix several minor bugs and make pip-pip more readable 	and enhance the test.sh script

--- test.sh
+++ test.sh
@@ -1,57 +1,102 @@
 #!/bin/sh
 
+if [ -f test.log ]; then
+    mv -f test.log test.log.bak > /dev/null 2>&1
+fi
+
+fail=false
+
+retval=
 execute () {
-    echo "TESTING -- $@"
-    $@
-    echo
+    $@ >> test.log 2>&1
+    retval=$?
 }
 
-execute ./pip-pip --help
-execute ./pip-pip --dryrun
+OK () {
+    echo "OK TESTING -- $@"
+    echo "OK TESTING -- $@" >> test.log
+    execute $@
+    if [ $retval == 0 ]; then
+	echo "   SUCCESS -- $@"
+	echo "SUCCESS -- $@" >> test.log
+    else
+	echo "****FAILED -- $@"
+	echo "FAILED -- $@" >> test.log
+	fail=true
+    fi
+}
 
-execute ./pip-pip --dryrun --how=yum
-execute ./pip-pip --dryrun --how=yum --sudo
-execute ./pip-pip --dryrun --how=docker
-execute ./pip-pip --dryrun --how=docker --sudo
-execute ./pip-pip --dryrun --how=spack
-execute ./pip-pip --dryrun --how=github
-execute ./pip-pip --dryrun --how=git-rccs
-execute ./pip-pip --dryrun --how=yum,docker --sudo
-execute ./pip-pip --dryrun --how=docker,spack
-execute ./pip-pip --dryrun --how=spack,github
-execute ./pip-pip --dryrun --how=github,git-rccs
-execute ./pip-pip --dryrun --how=git-rccs,yum
-execute ./pip-pip --dryrun --how=all
-execute ./pip-pip --dryrun --how=ALL
+NG () {
+    echo "NG TESTING -- $@"
+    echo "NG TESTING -- $@" >> test.log
+    execute $@
+    if [ $retval != 0 ]; then
+	echo "   SUCCESS -- $@"
+	echo "SUCCESS -- $@" >> test.log
+    else
+	echo "****FAILED -- $@"
+	echo "FAILED -- $@" >> test.log
+	fail=true
+    fi
+}
 
-execute ./pip-pip --dryrun --how=yum      --version=all --sudo
-execute ./pip-pip --dryrun --how=docker   --version=ALL --sudo
-execute ./pip-pip --dryrun --how=spack    --version=-1  --sudo
-execute ./pip-pip --dryrun --how=git-rccs --version=2   --sudo
-execute ./pip-pip --dryrun --how=github   --version=3   --sudo
+OK ./pip-pip --help
+OK ./pip-pip --dryrun
 
-execute ./pip-pip --dryrun --how=yum    --prefix=INSTALL --sudo
-execute ./pip-pip --dryrun --how=docker --prefix=INSTALL --sudo
-execute ./pip-pip --dryrun --how=spack  --prefix=INSTALL
-execute ./pip-pip --dryrun --how=github --prefix=INSTALL
-execute ./pip-pip --dryrun --how=github --prefix=INSTALL//
+OK ./pip-pip --dryrun --how=yum
+OK ./pip-pip --dryrun --how=yum --sudo
+OK ./pip-pip --dryrun --how=docker
+OK ./pip-pip --dryrun --how=docker --sudo
+OK ./pip-pip --dryrun --how=spack
+OK ./pip-pip --dryrun --how=github
+OK ./pip-pip --dryrun --how=git-rccs
+OK ./pip-pip --dryrun --how=yum,docker --sudo
+OK ./pip-pip --dryrun --how=docker,spack
+OK ./pip-pip --dryrun --how=spack,github
+OK ./pip-pip --dryrun --how=github,git-rccs
+OK ./pip-pip --dryrun --how=git-rccs,yum
+OK ./pip-pip --dryrun --how=all
+OK ./pip-pip --dryrun --how=ALL
 
-execute ./pip-pip --dryrun --how=yum    --work=WORK --sudo
-execute ./pip-pip --dryrun --how=docker --work=WORK --sudo
-execute ./pip-pip --dryrun --how=spack  --work=WORK
-execute ./pip-pip --dryrun --how=github --work=WORK
-execute ./pip-pip --dryrun --how=github --work=WORK
+OK ./pip-pip --dryrun --how=yum      --version=all --sudo
+OK ./pip-pip --dryrun --how=docker   --version=ALL --sudo
+OK ./pip-pip --dryrun --how=spack    --version=-1  --sudo
+OK ./pip-pip --dryrun --how=git-rccs --version=2   --sudo
+OK ./pip-pip --dryrun --how=github   --version=3   --sudo
 
-execute ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK
-execute ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK --noglibc
-execute ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK --nogdb
+OK ./pip-pip --dryrun --how=yum    --prefix=INSTALL --sudo
+OK ./pip-pip --dryrun --how=docker --prefix=INSTALL --sudo
+OK ./pip-pip --dryrun --how=spack  --prefix=INSTALL
+OK ./pip-pip --dryrun --how=github --prefix=INSTALL
+OK ./pip-pip --dryrun --how=github --prefix=INSTALL//
 
-execute ./pip-pip --dryrun --how=all --version=all
-execute ./pip-pip --dryrun --how=ALL --version=ALL
+OK ./pip-pip --dryrun --how=yum    --work=WORK --sudo
+OK ./pip-pip --dryrun --how=docker --work=WORK --sudo
+OK ./pip-pip --dryrun --how=spack  --work=WORK
+OK ./pip-pip --dryrun --how=github --work=WORK
+OK ./pip-pip --dryrun --how=github --work=WORK
 
-execute ./pip-pip --dryrun --how=all --version=all --quiet
-execute ./pip-pip --dryrun --how=ALL --version=ALL --keep
-execute ./pip-pip --dryrun --how=all --version=all --force
+OK ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK
+OK ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK --noglibc
+OK ./pip-pip --dryrun --how=github --prefix=INSTALL --work=WORK --nogdb
 
-execute ./pip-pip --dryrun --how=all --version=all --test=0
-execute ./pip-pip --dryrun --how=ALL --version=ALL --test=100
+OK ./pip-pip --dryrun --how=all --version=all
+OK ./pip-pip --dryrun --how=ALL --version=ALL
+
+OK ./pip-pip --dryrun --how=all --version=all --quiet
+OK ./pip-pip --dryrun --how=ALL --version=ALL --keep
+OK ./pip-pip --dryrun --how=all --version=all --force
+
+OK ./pip-pip --dryrun --how=all --version=all --test=0
+OK ./pip-pip --dryrun --how=ALL --version=ALL --test=100
+
+NG ./pip-pip  --dryrun --nosuchoption
+NG ./pip-pip --dryrun --how=unknown
+NG ./pip-pip --dryrun --version=9
+
+if $fail; then
+    exit 1
+fi
+
+echo "Succeeded"
+exit 0
